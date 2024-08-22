f342bf20162d2c5d29f29e9a58f96e925ed56a84
Madros Madros <149497364+madrosir@users.noreply.github.com>
2024-08-22 20:34:29 +0300

    last

--- components/post/nav/search.tsx
+++ components/post/nav/search.tsx
@@ -62,7 +62,7 @@ export default function Search() {
   return (
     <div className="relative">
       <Input
-        placeholder="Search Anime"
+        placeholder="Search People"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm text-black outline-2 placeholder:text-gray-500"
         onChange={(e) => handleSearch(e.target.value)}
         value={searchTerm}
